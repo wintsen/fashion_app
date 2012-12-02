22b7f3f2aac63dccfe7af84d17f9b925cd4fcde1
Vincent <wintsen@gmail.com>
2012-12-02 00:30:15 -0800

    Guard

--- Gemfile
+++ Gemfile
@@ -6,17 +6,22 @@ gem 'bcrypt-ruby', '3.0.1'
 gem 'faker', '1.0.1'
 gem 'will_paginate', '3.0.3'
 gem 'bootstrap-will_paginate', '0.0.6'
+gem 'jquery-rails', '2.0.2'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-group :development do
+group :development, :test do
     gem 'sqlite3', '1.3.5'
     gem 'rspec-rails', '2.11.0'
+    gem 'rb-fsevent', '0.9.1', :require => false
     gem 'guard-rspec', '1.2.1'
+    gem 'guard-spork', '1.2.0'
+    gem 'spork', '0.9.2'
+    gem 'capybara', '1.1.2'
+    gem 'factory_girl_rails', '4.1.0'
+    gem 'growl', '1.0.3'
 end
-
-gem 'annotate', '2.5.0', group: :development
 
 # Gems used only for assets and not required
 # in production environments by default.
@@ -26,30 +31,6 @@ group :assets do
   gem 'uglifier', '1.2.3'
 end
 
-gem 'jquery-rails', '2.0.2'
-
-group :test do
-    gem 'capybara', '1.1.2'
-    gem 'factory_girl_rails', '4.1.0'
-    gem 'rb-fsevent', '0.9.1', :require => false
-    gem 'growl', '1.0.3'
-end
-
 group :production do
     gem 'pg', '0.12.2'
 end
-
-# To use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
-
-# To use Jbuilder templates for JSON
-# gem 'jbuilder'
-
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Deploy with Capistrano
-# gem 'capistrano'
-
-# To use debugger
-# gem 'debugger'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
+require 'rubygems'
+require 'spork'
+#uncomment the following line to use spork with the debugger
+#require 'spork/ext/ruby-debug'
+
+Spork.prefork do
+  # Loading more in this block will cause your tests to run faster. However,
+  # if you change any configuration or code from libraries loaded here, you'll
+  # need to restart spork for it take effect.
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
@@ -16,6 +25,7 @@ RSpec.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
+  config.mock_with :rspec
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -29,10 +39,10 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+end
+end
 
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = "random"
+Spork.each_run do
+  # This code will be run each time you run your specs.
+
 end
