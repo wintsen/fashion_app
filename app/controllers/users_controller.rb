dd797dd67b834aa4c98f3bf88bf24e1cec55c75c
Vincent <wintsen@gmail.com>
2012-12-02 22:18:16 -0800

    Redirect to correct place after signin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -23,7 +26,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Account updated!"
       sign_in @user
@@ -31,5 +33,18 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  private
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -59,7 +59,10 @@ describe "User Pages" do
 
   describe "edit" do
     let(:user) {FactoryGirl.create(:user)}
-    before {visit edit_user_path(user)}
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
 
     describe "page" do
       it {should have_selector('h1', text: "Update your profile")}
